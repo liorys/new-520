34260edc01c663ebe703474a9b218a962938ec24
root <fabio.fugiyama@yahoo.com.br>
2018-06-29 21:38:18 -0300

    aula4

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-msg = 'Hello World!'
-nome = input('Digite seu nome\n')
-idade = input('Digite sua idade\n')
-print('Seja bem-vindo {}, voce tem {} anos de idade'.format(nome,idade))
+# msg = 'Hello World!'
+# nome = input('Digite seu nome\n')
+# idade = input('Digite sua idade\n')
+# print('Seja bem-vindo {}, voce tem {} anos de idade'.format(nome,idade))
+
+
+from time import sleep
+while True:
+    print('ola')
+    sleep(3)
